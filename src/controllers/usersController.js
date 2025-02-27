cc322e0f90ad91b96a34179027ef7976df25a9b8
Plamedi Ilunga Ilunga <2038993@cegeprdl.ca>
2025-02-27 15:36:51 -0500

    Ajout des dependances helmet pour sécuriser les entêtes et programmation de du register en cours...

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -15,3 +15,76 @@ exports.getUser = async function (req, res) {
         res.status(500).json({ message: "Erreur lors de la récupération des clients" });
     }
 }
+
+exports.getAppToken = async function (req, res) {
+    console.log("user/getAppToken");
+    console.log(process.env.CREATION_EMAIL);
+    console.log(process.env.CREATION_PASS);
+
+    const email = req.body.email;
+    const pass = req.body.pass;
+    const user = { uname: email };
+
+    // Validate user input
+    if (!(email && pass)) {
+        return res.status(400).send({ message: "All input is required" });
+    }
+
+    //Verifier si on l'authorise
+    if (email != process.env.CREATION_EMAIL || pass != process.env.CREATION_PASS)
+        return res.status(401).send({ message: "Ask administrator for access" });
+
+    const accessToken = jwtUtil.generateAppToken(user);
+    res.status(200).json({ AppToken: accessToken });
+};
+
+exports.login = async function (req, res) {
+    try {
+      const { email, password } = req.body;
+      console.log(email + " + " + password);
+  
+      const connection = await connectToBD();
+  
+      if (!(email && password)) {
+        return res.status(400).send({ message: "Tout les champs sont requis" });
+      }
+  
+      let Tempemail = sanitizeField(email);
+      let TempPassword = sanitizeField(password);
+  
+      if (email != Tempemail || password != TempPassword) {
+        console.log("Champs invalide");
+        return res
+          .status(400)
+          .send({
+            message:
+              "Champs invalide. Veuillez vérifier les caractère spéciaux et veuillez réesayer !",
+          });
+      }
+  
+         const [rows] = await connection.execute(
+                'SELECT * FROM Utilisateur WHERE adresseCourrielUtilisateur = ?',
+                [email] // Paramètre à passer pour l'email
+          );
+      if ([rows] == null) {
+        res.status(409);
+        res.send({
+          message: "Erreur lors de la sélection de l'usager. Veuillez reesayer",
+        });
+      } else {
+        const user = rows[0];
+        if (user && (await bcrypt.compare(password, user.motDePasseUtilisateur))) {
+          const bearerToken = jwtUtil.generateAccessToken(
+            user.adresseCourrielUser
+          );
+          idUtilisateur = user.idUtilisateur;
+          console.log("idUtilisateur: " + idUtilisateur);
+          res.json({ bearerToken , idUtilisateur});
+        } else {
+          res.status(400).send({ message: "Courriel ou mot de passe invalide" });
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
